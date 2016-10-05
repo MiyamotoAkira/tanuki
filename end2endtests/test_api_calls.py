49a97a15cfa258ab78fb17eacaed06074764e41e
MiyamotoAkira <yefo.akira@gmail.com>
2016-10-05 08:16:47 +0100

    Added basis for test
    The files that will be used as the basis for tests has been added.
    We will be using python and the requests library.

---
+++ end2endtests/test_api_calls.py
@@ -0,0 +1,30 @@
+#
+# Call Flow
+# GET:host
+# Should return a list of all the possible options
+# This will be returned as hypermedia
+# Current options should be
+# GET:"/all" (whatever the url) will return you all the toggles
+# GET:"/group/{groupname}" will return you the toggles for that specific group
+# GET: "/on" will return you all the toggles that are currently on
+# GET: "/off" will return you all the toggles that are currently off.
+# POST: "/"
+# All the four above will accept a query parameter called "max_entries", which will limit the number of entries that will appear on the answer. If "max_entries" is specified links to "next", "previous", "first" and "last" will be provided
+# All the four above will be sorted alphabetically
+
+# Improvements
+# Provide a search option, maybe through a query parameter? or should be a sub resource?
+
+from requests import requests
+
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
+200
+r.headers['content-type']
+'application/json; charset=utf8'
+r.encoding
+'utf-8'
+r.text
+
+r.json()
