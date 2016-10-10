a3c67c2f92da14c47550ecb8a8c455e338ce434b
MiyamotoAkira <yefo.akira@gmail.com>
2016-10-10 08:26:29 +0100

    Completed basis for calls
    The initial set of requests and resources that will be provided have
    been described on the end2endtests test api calls file (will need to be
    moved later into the README.md document once the tests are fully setup
    and the application is working)

--- end2endtests/test_api_calls.py
+++ end2endtests/test_api_calls.py
@@ -5,12 +5,55 @@
 # This will be returned as hypermedia
 # Current options should be
 # GET:"/all" (whatever the url) will return you all the toggles
-# GET:"/group/{groupname}" will return you the toggles for that specific group
-# GET: "/on" will return you all the toggles that are currently on
+# GET:"/group/{groupResourceIdentifier}" will return you the toggles for that specific group
+# GET: "/on" will return you all the toggles that are currently on.
 # GET: "/off" will return you all the toggles that are currently off.
-# POST: "/"
-# All the four above will accept a query parameter called "max_entries", which will limit the number of entries that will appear on the answer. If "max_entries" is specified links to "next", "previous", "first" and "last" will be provided
-# All the four above will be sorted alphabetically
+
+# groupResourceIdentifier is calculated on the fly based on the possible options of collating groups
+# resourceIdentifer maybe on the fly as well?
+
+# All the above will accept a query parameter called "max_entries", which will limit the number of entries that will appear on the answer. If "max_entries" is specified links to "next", "previous", "first" and "last" will be provided.
+# A header request (or query parameter) of "embedded" will provide the full information of all the toggles -- Needs to be query parameter so it appears on the links when max_entries is present.
+# All the above will be sorted alphabetically
+
+
+# Results provided
+#
+# GroupResourceIdentifer (if present)
+# ToggleCollection:(for each)
+#    Name
+#    Status (on or off)
+#    Links:
+#        Self
+# Links:
+#    Self
+#    First (if max_entries was specified)
+#    Previous (if max_entries was specified)
+#    Next (if max_entries was specified)
+#    Last (if max_entries was specified)
+#
+# Embedded: (if requested)
+#     Toggles: (foreach)
+#         (Check single below)
+
+# POST: "/" Used to create a new toggle. By default it will be switched off
+# GET: "/{toggleId}"
+# POST: "/{toggleId}/switch
+
+# Results provided
+
+# For single
+# Name
+# Groups: (for each)
+#    Name
+# Status (on or off)
+# Links:
+#   Self
+#   Get
+#   Switch OFF
+#   or
+#   Switch ON
+#   Modify
 
 # Improvements
 # Provide a search option, maybe through a query parameter? or should be a sub resource?
