128ea36bd61f130b862f44132f95a796e1acf622
MiyamotoAkira <yefo.akira@gmail.com>
2018-10-14 17:02:25 +0100

    Eliminated all previous code and changed license

--- eu.twoormore.tanuki.api/test/eu/twoormore/tanuki/api/handler_test.clj
+++
@@ -1,7 +0,0 @@
-(ns eu.twoormore.tanuki.api-test
-  (:require [clojure.test :refer :all]
-            [eu.twoormore.tanuki.api :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
